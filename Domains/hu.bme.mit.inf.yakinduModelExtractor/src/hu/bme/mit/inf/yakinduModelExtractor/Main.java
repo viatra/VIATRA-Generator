267ada8df5c5cbeec194b499450dac6d4495f3fd
OszkarSemerath <oszka@152.66.252.189>
2017-07-05 16:41:03 +0200

    Added progress and basic statstic report to the yakindu model translator.

--- Domains/hu.bme.mit.inf.yakinduModelExtractor/src/hu/bme/mit/inf/yakinduModelExtractor/Main.java
+++ Domains/hu.bme.mit.inf.yakinduModelExtractor/src/hu/bme/mit/inf/yakinduModelExtractor/Main.java
@@ -13,6 +13,9 @@ public class Main {
 		ModelManager manager = new ModelManager();
 		Yakindu2CommonModel converter = new Yakindu2CommonModel();
 		
+		int models = 0;
+		int objects = 0;
+		
 		String years[] = {"2015","2016","2017"};
 		for(String year : years) {
 			String folderPath = "D:\\Eclipse\\GIT\\RemoHF\\"+year;
@@ -31,14 +34,31 @@ public class Main {
 				
 				if(s!=null) {
 					EObject s2 = converter.transform(s);
-					manager.saveModel(s2, "output/R"+year+index+".xmi");
+					//manager.saveModel(s2, "output/R"+year+index+".xmi");
+					int countObjects = countObjects(s2);
+					System.out.println(countObjects);
+					objects += countObjects;
+					models++;
 				}
-				System.out.println(year + " Progress: " + 100.0*index/paths.size() + "%");
+				//System.out.println(year + " Progress: " + 100.0*index/paths.size() + "%");
 				index++;
 			}
 			
 			System.out.println(year + " done.");
 		}
 		System.out.println("All done.");
+		System.out.println(objects);
+		System.out.println(models);
+		System.out.println(1.0*objects/models);
+	}
+	
+	private static int countObjects(EObject root) {
+		int objects = 1;
+		TreeIterator<EObject> i = root.eAllContents();
+		while(i.hasNext()) {
+			i.next();
+			objects++;
+		}
+		return objects;
 	}
 }
