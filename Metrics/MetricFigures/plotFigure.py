7c5ef88586d9d82d313a42cbaff870b6739792da
ArenBabikian <aren.babikian@mail.mcgill.ca>
2019-07-11 01:01:39 -0400

    REALMET : implement further plots for SQR

--- Metrics/MetricFigures/plotFigure.py
+++ Metrics/MetricFigures/plotFigure.py
@@ -17,36 +17,44 @@ def main():
 			, 'VS-WF+All7'
 			, 'VS+i'
 			]
-	metrics = ['SQRtot']
-	
-	for metricName in metrics :
-		for sourceName in sources:
+	numSources = len(sources)
+
+	metrics = ['SQRmax']
+
+	allSources = [[] *  numSources for i in range(numSources)]
+	allSourcesX = [[] *  numSources for i in range(numSources)]
 		
+	for metricName in metrics :
+		srcInd = 0
+		for sourceName in sources:
 			csvLocation = '../ca.mcgill.ecse.dslreasoner.realistic.metrics/outputs/calculatedMetrics/'
 			csvFileName = sourceName + '/' + metricName
+
+			#Make paths if inexistant
+			dirPathName = 'outputs/' + sourceName
+			pathName = dirPathName + '/' + metricName + ".png"
+			createFiles(dirPathName, pathName)
+
+			#Manipulations
 			with open(csvLocation + csvFileName + '.csv') as f:
 				reader = csv.reader(f, delimiter=',')
 				ind = 0
 				for row in reader:
 					rowName = row[0] + row[1]
-					for i in row[2:] :
-						#print(i)
-						float(i)
 					rowVals = [float(i) for i in row[2:]]
+					allSources[srcInd] = [i for i in rowVals]	
+					#Operations
+					rowVals.sort()
+					maxVal = max(rowVals)
+					rowVals = [i/maxVal for i in rowVals]					
+					#End Operations
 					if ind == 0 :
 						plt.plot(rowVals, label = rowName, lineWidth = 3)
 					else :
 						plt.plot(rowVals, label = rowName, alpha = 0.5)
-					ind += 1
+					ind += 1			
 
-			#Make paths if inexistant
-			dirPathName = 'outputs/' + sourceName
-			pathName = 'outputs/' + csvFileName + ".png"
-			if not path.exists(dirPathName) :
-				mkdir(dirPathName)
-			if not path.exists(pathName) :
-				open(pathName, 'a').close()
-		
+			#INDIVIDUAL FIGURES
 			#plt.title("Node Activity measurement w/ and w/o neighbourhood")
 			plt.title(metricName + " measurement for " + sourceName)
 			plt.ylabel("Metric value")
@@ -54,9 +62,64 @@ def main():
 		#	plt.ylim(bottom=0, top=5)
 			plt.legend()
 			plt.savefig(pathName)
-			plt.show()
+			#plt.show()
 			plt.clf()
-		#ADD COMPILATION OF SOURCES FOR A GIVEN METRIC HERE!!!!
+
+			srcInd += 1
+
+		#COMPILATION FIGURE
+		#Make paths if inexistant
+		dirPathName = 'outputs/_Cumulative'
+		pathName = dirPathName + '/' + metricName + ".png"
+		createFiles(dirPathName, pathName)
+
+		maxModels = 0
+		maxMetVal = 0
+
+		for i in range(numSources) :
+			allSources[i].sort()
+			allSources[i] = [j for j in allSources[i]]
+
+			###for normalizing number of models
+			numModels = len(allSources[i])
+			if numModels > maxModels :
+				maxModels = numModels
+
+			###for normalizing metric values
+			locMaxMetVal = max(allSources[i])
+			if locMaxMetVal > maxMetVal :
+				maxMetVal = locMaxMetVal
+		
+		for i in range(numSources) :
+			numModels = len(allSources[i])
+
+			###normalize number of models to (max number of models, 1)
+			#allSourcesX[i] = np.arange(0, maxModels, float(maxModels) / numModels) 
+			allSourcesX[i] = np.arange(0, 1, 1 / numModels) 
+
+			###normalize metric values to (max Metric Value, 1)
+			currentMax = max(allSources[i])
+			#allSources[i] = [j * maxMetVal / currentMax for j in allSources[i]]
+			#allSources[i] = [j / currentMax for j in allSources[i]]
+
+
+			plt.plot(allSources[i], allSourcesX[i], label = sources[i], lineWidth = 3)
+		
+		plt.title(metricName + " measurement for all sources")
+		plt.ylabel("Metric value")
+		plt.xlabel("Different models")
+	#	plt.ylim(bottom=0, top=5)
+		plt.legend()
+		plt.savefig(pathName)
+		plt.show()
+		plt.clf()
+
+
+def createFiles(dirPathName, pathName) :
+	if not path.exists(dirPathName) :
+		mkdir(dirPathName)
+	if not path.exists(pathName) :
+		open(pathName, 'a').close()
 
 	
 if __name__== "__main__":
