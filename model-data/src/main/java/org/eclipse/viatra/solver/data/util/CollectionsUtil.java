2ff73d68f76d873b783175a50d80626c0be7f1ef
OszkarSemerath <semerath@mit.bme.hu>
2021-08-15 15:17:53 +0200

    Added filter

--- model-data/src/main/java/org/eclipse/viatra/solver/data/util/CollectionsUtil.java
+++ model-data/src/main/java/org/eclipse/viatra/solver/data/util/CollectionsUtil.java
@@ -1,7 +1,9 @@
 package org.eclipse.viatra.solver.data.util;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public final class CollectionsUtil {
 	private CollectionsUtil() {
@@ -25,5 +27,46 @@ public final class CollectionsUtil {
 	
 	static <S,T> Iterable<T> map(Iterable<S> source, Function<S, T> transformation) {
 		return (()->map(source.iterator(),transformation));
+	}
+	
+	static <T> Iterator<T> filter(Iterator<T> source, Predicate<T> condition) {
+		return new Iterator<T>() {
+			T internalNext = move();
+			boolean internalHasNext;
+			
+			private T move() {
+				internalHasNext = source.hasNext();
+				if(internalHasNext) {
+					internalNext = source.next();
+				}
+				while(internalHasNext && !condition.test(internalNext)) {
+					internalHasNext = source.hasNext();
+					if(internalHasNext) {
+						internalNext = source.next();
+					}
+				}
+				return internalNext;
+			}
+
+			@Override
+			public boolean hasNext() {
+				return internalHasNext;
+			}
+
+			@Override
+			public T next() {
+				if(!internalHasNext) {
+					throw new NoSuchElementException();
+				} else {
+					T result = internalNext;
+					move();
+					return result;
+				}
+			}
+		};
+	}
+	
+	static <T> Iterable<T> filter(Iterable<T> source, Predicate<T> condition) {
+		return (()->filter(source.iterator(),condition));
 	}
 }
